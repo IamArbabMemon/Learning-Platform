e3667da64e29e2f7f906c5a2cd22ce54598b6b29
IamArbabMemon <139270412+IamArbabMemon@users.noreply.github.com>
2024-07-01 15:03:22 -0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import {sum as add} from './controllers/student.controller'
+
+console.log(add(2,5));
---
+++ src/models/student.model.ts
@@ -0,0 +1,11 @@
+/*
+
+1 . student collection 
+2 . teachers collection
+3 . course collection
+4 . student_login collection
+5  . teacher login collection
+6  . student_Enrollment collection
+
+
+*/
