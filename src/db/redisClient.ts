575b565c0d887abdb600c1c313817df3b6161424
IamArbabMemon <139270412+IamArbabMemon@users.noreply.github.com>
2024-09-26 15:06:23 -0700

    'commit'

--- src/db/redisClient.ts
+++ src/db/redisClient.ts
@@ -1,14 +1,16 @@
 // import redis from 'redis';
 import { createClient } from 'redis';
 
-export const redisClient = createClient({
+const redisClient = createClient({
     // host: process.env.REDIS_HOST || 'localhost',
     // port: process.env.REDIS_PORT || 6379,
     // password: process.env.REDIS_PASSWORD, // If your Redis instance requires authentication
       url: `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`
 });
 
-
+export {
+  redisClient
+}
 
 
 // redisClient.on('error', (err) => {
